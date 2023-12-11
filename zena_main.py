2e00772b28080f8082e92ec0de719948ce2ff0c4
Siddhant Kumar Vishwakarma <66521131+siddseoni@users.noreply.github.com>
2023-12-11 10:27:25 +0530

    Update zena_main.py

--- zena_main.py
+++ zena_main.py
@@ -1,8 +1,3 @@
-# print("hello World")
-# import streamlit
-
-# streamlit.title("My Parents New Healthy Diner")
-
 import streamlit
 import snowflake.connector
 import pandas
@@ -15,15 +10,16 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
+print(df)
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 # put the first column into a list
-color_list = df[0].values.tolist()
+# color_list = df[0].values.tolist()
 # print(color_list)
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+# option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 # We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+# product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 # my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 # df2 = my_cur.fetchone()
